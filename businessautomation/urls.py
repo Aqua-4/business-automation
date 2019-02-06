e5760f0e0c8822af4ef97bb0ff11f62505325431
Aqua-4 <punks23not8dead@gmail.com>
2019-02-06 22:18:53 +0530

    WIP: login

--- businessautomation/urls.py
+++ businessautomation/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'),
+        {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.LogoutView,
+        {'template_name': 'logged_out.html'}, name='logout'),
 ]
